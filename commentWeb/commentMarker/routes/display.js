dfdddf54a2d93540d972291971329c5e77d5f626
Jingyi Yuan <jingyy3@cs.washington.edu>
2018-05-15 03:42:09 -0700

    display need to fix rendering

--- commentWeb/commentMarker/routes/display.js
+++ commentWeb/commentMarker/routes/display.js
@@ -1,80 +1,62 @@
 var express = require('express');
 var router = express.Router();
 var postNum = 1;
+var cursor = null;
 
-
-
-//Currently hard coding collection name; ideally take as input
 
 router.get('/', function(req, res, next) {
   const db = req.app.locals.db;
   let posts = req.app.locals.collection;
 
-  var startId = req.query.postStartID;
-  var scoreThreshold = req.query.score;
+  const startId = +req.query.postStartID;
+  const scoreThreshold = +req.query.score;
   var sortScore = req.query.sortScore;
   var sortID = req.query.sortID;
   var zeroComments = req.query.zeroComments;
-
-
+  
   //create index
   posts.createIndex('Id', function(err){
     if (err) throw err;
   });
 
-  //if sort post id is chosen 
-
-  if (sortID === "True") {
-    console.log("HERE");
-    posts.find({Id:{'$gte':startId}, Score:{'$lte':scoreThreshold}}).sort({Id : 1}).limit(1000).toArray((err, result) => {
+  if(sortID === "True"){
+    cursor = posts.find({"Id":{$gte:startId}, "Score": {$lte:scoreThreshold}}).sort({Id : 1}).limit(1000);
+    cursor.each(function(err, result){
       if (err) throw err;
-      let len = result.length;
-      var postCollection = [];
-  
-      var morePost = (len === (postNum + 1));
-      var nextPostId = morePost ? result[postNum].postid : -1;
-      var partial = morePost ? result.slice(0, postNum) : result;
-  
-      for(let i = 0; i < partial.length; i++){
-        console.log("Retrieved");
-        var singlePost = {};
-        singlePost.Id = result[i].Id;
-        singlePost.Score = result[i].Score;
-        singlePost.commentCount = result[i].commentCount;
-        singlePost.Body = result[i].Body;
-        singlePost.Comments = result[i].Comments;
-  
-        postCollection.push(singlePost);
+      if(result == null){
+        console.log("NULL");
       }
-  
-      res.render('display', {post: postCollection, nextId: nextPostId});
-    })
-
+      var postCollection = [];
+      var singlePost = {};
+      singlePost.Id = result['Id'];
+      singlePost.Score = result['Score'];
+      singlePost.commentCount = result['commentCount'];
+      singlePost.Body = result['Body'];
+      singlePost.Comments = result['Comments'];
+      postCollection.push(singlePost);
+      res.render('display', {post: postCollection});
+    });
+    
   }else if(sortScore === "True"){
-    posts.find({Id:{'$gte':startId}, Score:{'$lte':scoreThreshold}}).sort({score : -1}).limit(1000).toArray((err, result) => {
-      if (err) throw err;
-      let len = result.length;
-      var postCollection = [];
-  
-      var morePost = (len === (postNum + 1));
-      var nextPostId = morePost ? result[postNum].postid : -1;
-      var partial = morePost ? result.slice(0, postNum) : result;
-  
-      for(let i = 0; i < partial.length; i++){
-        var singlePost = {};
-        singlePost.Id = result[i].Id;
-        singlePost.Score = result[i].Score;
-        singlePost.commentCount = result[i].commentCount;
-        singlePost.Body = result[i].Body;
-        singlePost.Comments = result[i].Comments;
-  
-        postCollection.push(singlePost);
+    cursor = posts.find({Id:{'$gte':startId}, Score: {'$lte':scoreThreshold}}).sort({Score: -1}).limit(1000);
+    cursor.each(function(err, result){
+      if(err) throw err;
+      if(result == null){
+        console.log("NULL");
       }
-  
-      res.render('display', {post :postCollection, nextId: nextPostId});
-    })
+      var postCollection = [];
+      var singlePost = {};
+      singlePost.Id = result['Id'];
+      singlePost.Score = result['Score'];
+      singlePost.commentCount = result['commentCount'];
+      singlePost.Body = result['Body'];
+      singlePost.Comments = result['Comments'];
+      postCollection.push(singlePost);
+
+      res.render('display', {post: postCollection});
+      
+    });
   }
-  
 });
 
 module.exports = router;
