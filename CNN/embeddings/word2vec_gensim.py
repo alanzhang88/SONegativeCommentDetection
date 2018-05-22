cbb08512cd8daeaaa8692a67bef2d5d30e7c1757
Shuang Zhang <alanzhang88@gmail.com>
2018-05-22 16:16:16 -0700

    add false positive metric

--- CNN/embeddings/word2vec_gensim.py
+++ CNN/embeddings/word2vec_gensim.py
@@ -21,7 +21,7 @@ def word2vec_gen(fileName):
     data = pd.read_csv(fileName,names=['Comment','Label'])
     arr = data['Comment'].values
     tokenarr = [l.split() for l in arr]
-    model = gensim.models.word2vec.Word2Vec(tokenarr,min_count=2)
+    model = gensim.models.word2vec.Word2Vec(tokenarr,min_count=2,size=100)
     model.save(fname_or_handle='./word2vec_model')
     model.wv.save_word2vec_format('./word2vec_vec')
     print('Finished model generation')
@@ -47,4 +47,3 @@ def get_embedding(fileName='./word2vec_vec'):
 
 if __name__ == '__main__':
     word2vec_gen('./processed.csv')
-
--- CNN/keras_text_CNN.py
+++ CNN/keras_text_CNN.py
@@ -3,6 +3,7 @@ from keras.layers import Embedding, Input, Conv2D, MaxPooling2D, Concatenate, Dr
 from keras.optimizers import Adam
 from keras.losses import categorical_crossentropy
 from data_generator import DataHandler
+import tensorflow as tf
 import warnings
 warnings.filterwarnings('ignore')
 from embedding_config import config
@@ -36,15 +37,19 @@ embedding_matrix = data.get_embedding_matrix()
 filter_sizes = [4,5,6,7]
 
 num_filters = 32
-#num_filters = 512 
+#num_filters = 512
 drop_prob = 0.2
 #drop_prob = 0.1
 lr = 0.001
 
+def fpp(y_true,y_pred):
+    mat = tf.confusion_matrix(labels=tf.argmax(y_true,1),predictions=tf.argmax(y_pred,1),num_classes=num_classes)
+    return mat[0][1] / (mat[0][1] + mat[1][1])
+
 inp = Input(shape=(maxlength,))
 x = Embedding(input_dim=embedding_matrix.shape[0],output_dim=embed_size,input_length=maxlength,weights=[embedding_matrix])(inp)
 
-x = SpatialDropout1D(0.4)(x)
+# x = SpatialDropout1D(0.4)(x)
 
 x = Reshape((maxlength,embed_size,1))(x)
 pooled_output = []
@@ -58,7 +63,7 @@ z = Flatten()(z)
 z = Dropout(rate=drop_prob)(z)
 outp = Dense(num_classes,kernel_initializer='random_normal',activation='sigmoid')(z)
 model = Model(inputs=inp,outputs=outp)
-model.compile(optimizer=Adam(lr=lr),loss=categorical_crossentropy,metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=lr),loss=categorical_crossentropy,metrics=['accuracy',fpp])
 
 batch_size = 128
 steps = 1000
@@ -70,8 +75,10 @@ for i in range(steps):
     if i % 100 == 0:
         res = model.test_on_batch(X_test,y_test)
         print('On step %d' % i)
-        print('Accuracy: %f \n' % res[1])
+        print('Accuracy: %f' % res[1])
+        print('FPP: %f \n' % res[2])
 
 res = model.test_on_batch(X_test,y_test)
 print('On step %d' % 1000)
-print('Accuracy: %f \n' % res[1])
+print('Accuracy: %f' % res[1])
+print('FPP: %f \n' % res[2])
