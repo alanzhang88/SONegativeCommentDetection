aa00922a9102c151021886a48fe7f41d23580d7e
Shuang Zhang <alanzhang88@gmail.com>
2018-05-23 18:39:56 -0700

    fixed savemodel

--- CNN/saveModel.py
+++ CNN/saveModel.py
@@ -6,12 +6,12 @@ class SaveModel(Callback):
         self.validation_data = validation_data
         self.target_val = target_val
         self.target_name = target_name
+        self.last = -1
 
     def on_epoch_end(self,epoch,logs={}):
         res = self.model.test_on_batch(self.validation_data[0],self.validation_data[1])
-        last = -1
         for i in range(len(self.model.metrics_names)):
-            if self.model.metrics_names[i] == self.target_name and res[i] >= self.target_val and res[i] > last:
-                last = res[i]
+            if self.model.metrics_names[i] == self.target_name and res[i] >= self.target_val and res[i] > self.last:
+                self.last = res[i]
                 print('Saveing model with %s reaching %f' % (self.target_name,res[i]))
                 self.model.save(filepath='./CNNmodel.h5')
