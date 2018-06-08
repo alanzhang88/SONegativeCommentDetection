4b92e4a221b3c5356b54102d4af4c6c7d427b6a4
Jingyi Yuan <jingyy3@cs.washington.edu>
2018-06-08 15:44:25 -0700

    update experiments

--- CNN/run_experiments.sh
+++ CNN/run_experiments.sh
@@ -1,9 +1,9 @@
-declare -a num_filters=(256 512)
+declare -a num_filters=(32)
 declare -a filter_sizes
-filter_sizes[0]="1,2,3,4,5"
-filter_sizes[0]="3,4,5,6,7"
+filter_sizes[0]="1,2,3,4"
+# filter_sizes[0]="3,4,5,6"
 
-declare -a epochs=(4 5)
+declare -a epochs=(3)
 
 for i in "${num_filters[@]}"
 do
@@ -11,16 +11,8 @@ do
 do 
         for k in "${epochs[@]}"
 do
-            python testParam.py -nf $i -fs $j -e $k >> ./experiments/log_$i$j$k.txt
+            python testParam.py -nf $i -fs $j -e $k | tail -n 1 | tee -a ./experiments/log_$i$j$k.txt 
+            
         done
     done
-done
-
-draw graph
-
-cd ./experiments
-
-for i in *.txt
-do
-    tail -n 1 $i >> results.txt
 done
