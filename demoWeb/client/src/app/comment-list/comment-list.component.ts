111979e333e3248bc376b5506b52adc19c5aaa28
Shuang Zhang <alanzhang88@gmail.com>
2018-06-08 12:53:45 -0700

    fixed progress bar

--- demoWeb/client/src/app/comment-list/comment-list.component.ts
+++ demoWeb/client/src/app/comment-list/comment-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-comment-list',
@@ -10,11 +11,19 @@ export class CommentListComponent implements OnInit {
   @Input() commentList = null;
   @Input() modelSelection = null;
 
-  constructor() { }
+  constructor(private sanitizer:DomSanitizer) { }
 
   ngOnInit() {
   }
 
+
+  getStyle(value){
+    return this.sanitizer.bypassSecurityTrustStyle(`width: ${Math.round(value*100)}%`);
+  }
+
+  getHtml(value){
+    return this.sanitizer.bypassSecurityTrustHtml(value);
+  }
 
   getValue(value){
     return Math.round(value*100);
