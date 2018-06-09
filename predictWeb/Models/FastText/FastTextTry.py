0a7202fce0dff61e13711d29f52bfce39856cbb6
duanli <dxl360@case.edu>
2018-06-08 23:57:43 -0700

    replace predict web

--- predictWeb/Models/FastText/FastTextTry.py
+++
@@ -1,9 +0,0 @@
-from FastTextUtil import FastText
-import pandas as pd
-import numpy as np
-
-instance = FastText()
-(train, test, trainlabel, testlabel) = instance.preprocessData()
-texts = instance.extractText(test)
-labels = instance.predict(texts)
-print(labels)
