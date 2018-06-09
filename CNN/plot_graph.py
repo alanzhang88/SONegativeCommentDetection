2c2dcffc37693d57ced8a3baf7176e1b4718a399
Jingyi Yuan <jingyy3@cs.washington.edu>
2018-06-09 00:33:33 -0700

    plot for param combo

---
+++ CNN/plot_graph.py
@@ -0,0 +1,36 @@
+import argparse
+import matplotlib.pyplot as plt
+import glob
+
+
+#Get all log files
+files = glob.glob('*.txt')
+
+print (files)
+
+
+
+
+
+
+
+# with open('./experiments/' + fileName, "r") as f:
+#     line = f.readline()
+#     line_arr= line.split("- ")
+#     acc = line_arr[6].split(":")[1]
+
+
+# x = 'Combination1'
+
+# plt.xlabel("Hyperparameters Combination")
+# plt.ylabel('Accuracy')
+# plt.plot(x,acc,'ro--')
+# plt.show()
+    
+    
+
+
+
+
+
+
