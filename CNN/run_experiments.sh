263d331ce0e406c1ddba760d7f056f6d1ec1f951
Jingyi Yuan <jingyy3@cs.washington.edu>
2018-06-08 18:54:55 -0700

    changed experiments setting

--- CNN/run_experiments.sh
+++ CNN/run_experiments.sh
@@ -1,7 +1,7 @@
-declare -a num_filters=(32)
+declare -a num_filters=(32 64)
 declare -a filter_sizes
 filter_sizes[0]="1,2,3,4"
-# filter_sizes[0]="3,4,5,6"
+
 
 declare -a epochs=(3)
 
@@ -11,8 +11,11 @@ do
 do 
         for k in "${epochs[@]}"
 do
-            python testParam.py -nf $i -fs $j -e $k | tail -n 1 | tee -a ./experiments/log_$i$j$k.txt 
-            
+            python testParam.py -nf $i -fs $j -e $k | tail -n 1 | tee -a ./experiments/log_$i+$j+$k.txt 
         done
     done
 done
+
+
+cd ./experiments
+python plot_graph.py
