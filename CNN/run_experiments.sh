0f7fb5e9b4f391da7dbb4b158940a38332d8483d
Jingyi Yuan <jingyy3@cs.washington.edu>
2018-06-09 12:31:46 -0700

    param tuning

--- CNN/run_experiments.sh
+++ CNN/run_experiments.sh
@@ -1,9 +1,9 @@
-declare -a num_filters=(32 64 128 256 512)
+declare -a num_filters=(32)
 declare -a filter_sizes
 filter_sizes[0]="1,2,3,4"
-filter_sizes[1]="2,3,4,5,6"
+# filter_sizes[1]="2,3,4,5,6"
 
-declare -a lr=(0.1 0.2 0.3)
+declare -a dp=(0.1 0.3 0.5 0.7 0.9)
 
 
 declare -a epochs=(3)
@@ -12,10 +12,10 @@ for i in "${num_filters[@]}"
 do
     for j in "${filter_sizes[@]}"
 do 
-        for k in "${lr[@]}"
+        for k in "${dp[@]}"
         
 do
-            python testParam.py -nf $i -fs $j -lr $k | tail -n 1 | tee -a ./experiments/log_$i+$j+$k.txt 
+            python testParam.py -nf $i -fs $j -dp $k | tail -n 1 | tee -a ./experiments/log_$i+$j+$k.txt 
         done
     done
 done
