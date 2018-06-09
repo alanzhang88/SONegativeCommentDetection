3074c763e354118ae27bed2be3f00d04d5be75b9
Jingyi Yuan <jingyy3@cs.washington.edu>
2018-06-09 00:02:11 -0700

    update experiments methodology

--- CNN/run_experiments.sh
+++ CNN/run_experiments.sh
@@ -1,6 +1,9 @@
-declare -a num_filters=(32 64)
+declare -a num_filters=(32 64 128 256 512)
 declare -a filter_sizes
 filter_sizes[0]="1,2,3,4"
+filter_sizes[1]="2,3,4,5,6"
+
+declare -a lr=(0.1 0.2 0.3)
 
 
 declare -a epochs=(3)
@@ -9,9 +12,10 @@ for i in "${num_filters[@]}"
 do
     for j in "${filter_sizes[@]}"
 do 
-        for k in "${epochs[@]}"
+        for k in "${lr[@]}"
+        
 do
-            python testParam.py -nf $i -fs $j -e $k | tail -n 1 | tee -a ./experiments/log_$i+$j+$k.txt 
+            python testParam.py -nf $i -fs $j -lr $k | tail -n 1 | tee -a ./experiments/log_$i+$j+$k.txt 
         done
     done
 done
